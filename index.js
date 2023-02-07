38a9a9ffba3b693ba1128906bdf21fcea79a1d66
choyon88 <choyonkhan88@gmail.com>
2023-02-07 22:31:22 +0600

    done

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-/*
+/* mindGame() function firstly checking its parameter number or string,
+if string then give error message.
+then again check the number is positive or negative, if negative then its give 
+error message, here result calculate by following precedence rules. */
+
 function mindGame(number) {
     // checking Number or String. 
     if (typeof number == 'number') {
@@ -15,15 +19,11 @@ function mindGame(number) {
         return 'Please Remove String';
   }
  }
- console.log(mindGame(5));
-mindGame() function firstly checking its parameter number or string,
-if string then give error message.
-then again check the number is positive or negative, if negative then its give 
-error message, here result calculate by following precedence rules. 
 
-*/
 
-/*
+
+ /* evenOdd() function take string input then it will check that number or string, if string then it will execute the program , else it will give an error. this function check overall input string length then modulo with 2 because checking even or odd. */
+
 function evenOdd(text) {
   //checking String or Number 
   if (typeof text === 'string') {
@@ -38,11 +38,10 @@ function evenOdd(text) {
  }
   }
 
-console.log(evenOdd('Batch7'));
 
-  */
+/* isLGSeven () this function check that the input number is bigger or smaller to 7 , if input number grater then 7 it will multiply input number with 2 , else it will subtract from 7. this function also check input number type string or number.
+*/
 
-/*
 function isLGSeven(number) {
     // checking Number or String. 
     if(typeof number == 'number'){
@@ -55,10 +54,9 @@ function isLGSeven(number) {
       return 'Please Enter Number !'
     }
    }
-   console.log(isLGSeven(-15));
-*/
 
-/*
+
+/* findingBadData function will take a parameter an array. this function has full validation of checking its array or not and also array input number or string, and this function check negative number and positive number here two empty array which contain badData as a negative value and goodData as a positive value. then badData length will be return. */
 
 function findingBadData(arr) {
     let badData = [];
@@ -84,12 +82,11 @@ function findingBadData(arr) {
     }
     return badData.length;
   }
-  console.log(findingBadData([-4, -9, -5, -33, -55]));
-
-*/
 
 
-/*
+/* gemsToDiamond () this function take three parameter as a number. then it will check its positive or negative number for validation if negative then it will give error message. then again it will check its number or string again string then it will give error message. this function can calculate the gems to diamond after the diamond grater then 2000 it will be subtract 2000 or it will normally return */
+
+
 function gemsToDiamond(number1, number2, number3) {
   // checking number is positive or negative.
   if ( number1 < 0 || number2 < 0 || number3 < 0){
@@ -98,7 +95,8 @@ function gemsToDiamond(number1, number2, number3) {
   // checking inputs number or string.
   else if (typeof number1 == 'string' || typeof number2 == 'string' || typeof number3 == 'string') {
     return 'Remove String';
-  }else {
+  } else {
+    // calculation gems to diamond. 
     const total = (number1 * 21) + (number2 * 32) + (number3 * 43);
     if (total >= 1000 * 2) {
       return total - 2000;
@@ -109,5 +107,3 @@ function gemsToDiamond(number1, number2, number3) {
     }
   }
 }
-console.log(gemsToDiamond(100, 5, 1));
-*/
--- testing.js
+++ testing.js
@@ -1,13 +1,20 @@
-function isLGSeven(number) {
-    // checking Number or String. 
-    if(typeof number == 'number'){
-     if (number > 7) {
-       return number * 2;
-     } else {
-       return number - 7;
-     }
+function gemsToDiamond(number1, number2, number3) {
+  // checking number is positive or negative.
+  if (number1 < 0 || number2 < 0 || number3 < 0) {
+    return 'Please Enter Positive Number';
+  }
+  // checking inputs number or string.
+  else if (typeof number1 == 'string' || typeof number2 == 'string' || typeof number3 == 'string') {
+    return 'Remove String Enter Number';
+  } else {
+    const total = (number1 * 21) + (number2 * 32) + (number3 * 43);
+    if (total > 1000 * 2) {
+      return total - 2000;
+
     } else {
-      return 'Please Enter Number !'
+      return total;
+
     }
-   }
-   console.log(isLGSeven(7));
+  }
+}
+console.log(gemsToDiamond(1, 1, 1));
